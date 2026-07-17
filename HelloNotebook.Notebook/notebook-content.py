cedcc0f9ba679571f769576aacabbf89ec5b9bf4
junichitms <49634828+junichitms@users.noreply.github.com>
2026-07-17 09:18:57 +0900

    feat: add cell from Fabric

--- HelloNotebook.Notebook/notebook-content.py
+++ HelloNotebook.Notebook/notebook-content.py
@@ -14,6 +14,7 @@
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
 print("Hello from Fabric → GitHub → VS Code! (edited locally)")
+print("Edited in Fabric workspace.")
 
 # METADATA ********************
 
