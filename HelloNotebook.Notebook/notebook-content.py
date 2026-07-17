f8255e6b95272414e44094867e52e613afa3b5b0
junichitms <49634828+junichitms@users.noreply.github.com>
2026-07-17 09:29:18 +0900

    Add hello notebook (#1)
    * chore: add gitignore
    
    * feat: add hello notebook
    
    * edit: tweak greeting from local
    
    * feat: add cell from Fabric
    
    ---------
    
    Co-authored-by: Junichi Tokunaga <junichit+microsoft@microsoft.com>

---
+++ HelloNotebook.Notebook/notebook-content.py
@@ -0,0 +1,24 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+print("Hello from Fabric → GitHub → VS Code! (edited locally)")
+print("Edited in Fabric workspace.")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
